64166dc3d58355ffc15c543a4306ee086c8921fc
Joyce Liu <jambajoyce@gmail.com>
2014-03-11 16:03:54 -0700

    scanning and poppin toasts

--- app/src/com/example/dcart/MainActivity.java
+++ app/src/com/example/dcart/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.dcart;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
 import android.view.Menu;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	private Button scanBtn;
+	private TextView formatTxt, contentTxt;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
+		scanBtn = (Button)findViewById(R.id.scan_button);
+		formatTxt = (TextView)findViewById(R.id.scan_format);
+		contentTxt = (TextView)findViewById(R.id.scan_content);
+		
+		scanBtn.setOnClickListener(this);
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
@@ -19,4 +39,26 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	
+	public void onClick(View v) {
+		if (v.getId() == R.id.scan_button) {
+			//scanning
+			IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+			scanIntegrator.initiateScan();
+		}
+	}
+	
+	// Retrieve Scan Results
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+		
+		if (scanningResult != null) {
+			
+		}
+		else {
+			Toast toast = Toast.makeText(getApplicationContext(), 
+					"Item not found in our database :(", Toast.LENGTH_SHORT);
+			toast.show();
+		}
+	}
 }
