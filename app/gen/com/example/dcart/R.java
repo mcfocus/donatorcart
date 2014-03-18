7046ad5b03bb79f6f16d10b63727cf2df05e2a4f
Joyce Liu <jambajoyce@gmail.com>
2014-03-17 21:06:47 -0700

    begin bluetooth process

--- app/gen/com/example/dcart/R.java
+++ app/gen/com/example/dcart/R.java
@@ -14,7 +14,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070004;
+        public static final int donate_button=0x7f070004;
+        public static final int menu_settings=0x7f070005;
         public static final int scan_button=0x7f070000;
         public static final int scan_content=0x7f070002;
         public static final int scan_format=0x7f070001;
@@ -28,6 +29,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int donate=0x7f040004;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int scan=0x7f040003;
--- app/src/com/example/dcart/MainActivity.java
+++ app/src/com/example/dcart/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.dcart;
 
 import android.os.Bundle;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
@@ -13,13 +14,40 @@ import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.UUID;
+  
+  
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+
 
 public class MainActivity extends Activity implements OnClickListener {
 
 	private Button scanBtn;
+	private Button donateBtn;
 	private TextView formatTxt;
 	private TextView contentTxt;
 	private TextView messageTxt;
+	
+	private static final String TAG = "bluetooth1";
+	
+	private BluetoothAdapter btAdapter = null;
+	private BluetoothSocket btSocket = null;
+	private OutputStream outStream = null;
+	
+	// SPP UUID service 
+	private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+	  
+	// MAC-address of Bluetooth module (arduino)
+	private static String address = "00:06:66:01:5E:DB";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +56,24 @@ public class MainActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.activity_main);
 		
 		scanBtn = (Button)findViewById(R.id.scan_button);
+		donateBtn = (Button)findViewById(R.id.donate_button);
+		
 		formatTxt = (TextView)findViewById(R.id.scan_format);
 		contentTxt = (TextView)findViewById(R.id.scan_content);
-		messageTxt = (TextView)findViewById(R.id.scan_message); 
+		messageTxt = (TextView)findViewById(R.id.scan_message);
+		
+		btAdapter = BluetoothAdapter.getDefaultAdapter();
+	    checkBTState();
 		
 		scanBtn.setOnClickListener(this);
+
+		
+	    donateBtn.setOnClickListener(new OnClickListener() {
+	        public void onClick(View v) {
+	          sendData("1");
+	          Toast.makeText(getBaseContext(), "Turn on LED", Toast.LENGTH_SHORT).show();
+	        }
+	      });
 		
 
 	}
@@ -88,4 +129,133 @@ public class MainActivity extends Activity implements OnClickListener {
 			toast.show();
 		}
 	}
+	
+	// Bluetooth
+
+	  private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+	      if(Build.VERSION.SDK_INT >= 10){
+	          try {
+	              final Method  m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", new Class[] { UUID.class });
+	              return (BluetoothSocket) m.invoke(device, MY_UUID);
+	          } catch (Exception e) {
+	              Log.e(TAG, "Could not create Insecure RFComm Connection",e);
+	          }
+	      }
+	      return  device.createRfcommSocketToServiceRecord(MY_UUID);
+	  }
+	   
+	  @Override
+	  public void onResume() {
+	    super.onResume();
+	 
+	    Log.d(TAG, "...onResume - try connect...");
+	   
+	    // Set up a pointer to the remote node using it's address.
+	    BluetoothDevice device = btAdapter.getRemoteDevice(address);
+	   
+	    // Two things are needed to make a connection:
+	    //   A MAC address, which we got above.
+	    //   A Service ID or UUID.  In this case we are using the
+	    //     UUID for SPP.
+	   
+		try {
+			btSocket = createBluetoothSocket(device);
+		} catch (IOException e1) {
+			errorExit("Fatal Error", "In onResume() and socket create failed: " + e1.getMessage() + ".");
+		}
+	    
+	    /*try {
+	      btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+	    } catch (IOException e) {
+	      errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+	    }*/
+	   
+	    // Discovery is resource intensive.  Make sure it isn't going on
+	    // when you attempt to connect and pass your message.
+	    btAdapter.cancelDiscovery();
+	   
+	    // Establish the connection.  This will block until it connects.
+	    Log.d(TAG, "...Connecting...");
+	    try {
+	      btSocket.connect();
+	      Log.d(TAG, "...Connection ok...");
+	    } catch (IOException e) {
+	      try {
+	        btSocket.close();
+	      } catch (IOException e2) {
+	        errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+	      }
+	    }
+	     
+	    // Create a data stream so we can talk to server.
+	    Log.d(TAG, "...Create Socket...");
+	 
+	    try {
+	      outStream = btSocket.getOutputStream();
+	    } catch (IOException e) {
+	      errorExit("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
+	    }
+	  }
+	 
+	  @Override
+	  public void onPause() {
+	    super.onPause();
+	 
+	    Log.d(TAG, "...In onPause()...");
+	 
+	    if (outStream != null) {
+	      try {
+	        outStream.flush();
+	      } catch (IOException e) {
+	        errorExit("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
+	      }
+	    }
+	 
+	    try     {
+	      btSocket.close();
+	    } catch (IOException e2) {
+	      errorExit("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
+	    }
+	  }
+	   
+	  private void checkBTState() {
+	    // Check for Bluetooth support and then check to make sure it is turned on
+	    // Emulator doesn't support Bluetooth and will return null
+	    if(btAdapter==null) { 
+	      errorExit("Fatal Error", "Bluetooth not supported");
+	    } else {
+	      if (btAdapter.isEnabled()) {
+	        Log.d(TAG, "...Bluetooth ON...");
+	      } else {
+	        //Prompt user to turn on Bluetooth
+	        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+	        startActivityForResult(enableBtIntent, 1);
+	      }
+	    }
+	  }
+	 
+	  private void errorExit(String title, String message){
+	    Toast.makeText(getBaseContext(), title + " - " + message, Toast.LENGTH_LONG).show();
+	    finish();
+	  }
+	 
+	  private void sendData(String message) {
+	    byte[] msgBuffer = message.getBytes();
+	 
+	    Log.d(TAG, "...Send data: " + message + "...");
+	 
+	    try {
+	      outStream.write(msgBuffer);
+	    } catch (IOException e) {
+	      String msg = "In onResume() and an exception occurred during write: " + e.getMessage();
+	      if (address.equals("00:00:00:00:00:00")) 
+	        msg = msg + ".\n\nUpdate your server address from 00:00:00:00:00:00 to the correct address on line 35 in the java code";
+	      	msg = msg +  ".\n\nCheck that the SPP UUID: " + MY_UUID.toString() + " exists on server.\n\n";
+	       
+	      	errorExit("Fatal Error", msg);       
+	    }
+	  }
+	
+	
+	
 }
